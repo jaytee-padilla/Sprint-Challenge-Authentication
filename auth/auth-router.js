45808b44d2b0818d46dd7f79beedd9da7f457a5f
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-23 09:10:11 -0700

    Added ability to GET users and register new users to database

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,38 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const usersDb = require('../users/users-model');
+
+// get users
+router.get('/users', (req, res) => {
+	usersDb.find()
+		.then(users => {
+			res.status(200).json(users);
+		})
+		.catch(error => {
+			res.status(500).json(error)
+		});
+});
 
 router.post('/register', (req, res) => {
-  // implement registration
+	// implement registration
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password);
+	user.password = hash;
+
+	usersDb.add(user)
+		.then(saved => {
+			res.status(201).json(saved);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+	// implement login
+	
 });
 
 module.exports = router;
